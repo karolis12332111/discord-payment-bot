5eec7dcfbafbd3f59c3b782a44c764e63004ad18
Karolis <topgturfas@gmail.com>
2026-01-14 14:15:34 +0200

    Add uptime web server

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 require('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Bot is alive 🚀');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Uptime server running on port ${PORT}`);
+});
+
 
 const {
   Client,
